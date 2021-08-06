f9083ecef6facd483a666dab4d02a4f1e1cd17dd
ROKA <bartender0n1y@gmail.com>
2021-08-06 17:12:44 +0900

    error, tags, part of dao update

--- MultiBoard/src/main/java/com/kosa/myapp/board/service/impl/BoardCategoryService.java
+++ MultiBoard/src/main/java/com/kosa/myapp/board/service/impl/BoardCategoryService.java
@@ -2,39 +2,42 @@ package com.kosa.myapp.board.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kosa.myapp.board.dao.IBoardCategoryRepository;
 import com.kosa.myapp.board.model.BoardCategory;
 import com.kosa.myapp.board.service.IBoardCategoryService;
 
+@Service
 public class BoardCategoryService implements IBoardCategoryService {
 
+	@Autowired
+	IBoardCategoryRepository boardCategoryRepository;
+	
 	@Override
 	public List<BoardCategory> selectAllCategory() {
-		// TODO Auto-generated method stub
-		return null;
+		return boardCategoryRepository.selectAllCategory();
 	}
 
 	@Override
 	public List<BoardCategory> selectAllCategoryByClass1(int class1) {
-		// TODO Auto-generated method stub
-		return null;
+		return boardCategoryRepository.selectAllCategoryByClass1(class1);
 	}
 
 	@Override
 	public void insertNewCategory(BoardCategory boardCategory) {
-		// TODO Auto-generated method stub
-		
+		boardCategory.setCategoryId(boardCategoryRepository.selectMaxCategoryId());
+		boardCategoryRepository.insertNewCategory(boardCategory);
 	}
 
 	@Override
 	public void updateCategory(BoardCategory boardCategory) {
-		// TODO Auto-generated method stub
-		
+		boardCategoryRepository.updateCategory(boardCategory);
 	}
 
 	@Override
 	public void deleteCategory(int categoryId) {
-		// TODO Auto-generated method stub
-		
+		boardCategoryRepository.deleteCategory(categoryId);
 	}
-
 }
--- MultiBoard/src/main/java/com/kosa/myapp/board/service/impl/BoardService.java
+++ MultiBoard/src/main/java/com/kosa/myapp/board/service/impl/BoardService.java
@@ -2,112 +2,149 @@ package com.kosa.myapp.board.service.impl;
 
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.kosa.myapp.board.model.Board;
 import com.kosa.myapp.board.model.BoardUploadFile;
 import com.kosa.myapp.board.service.IBoardService;
 
+@Service
 public class BoardService implements IBoardService {
-
-	@Override
+	
+//	@Autowired
+//	Iqwerty qwerty;
+	Board board = new Board();
+	
+	
+	@Transactional
 	public void insertArticle(Board boardId) {
-		// TODO Auto-generated method stub
-		
+		board.setBoardId(qwerty.selectMaxArticleNo()+1);
+		qwerty.insertArticle(board);
 	}
 
-	@Override
+	@Transactional
 	public void insertArticle(Board boardId, BoardUploadFile file) {
-		// TODO Auto-generated method stub
-		
+		board.setBoardId(qwerty.selectMaxArticleNo()+1);
+		qwerty.insertArticle(board);
+		if(file != null && file.getFileName() != null &&
+	!file.getFileName().equals("")) {
+			file.setBoardId(board.getBoardId());
+			file.setFileId(qwerty.selectMaxFileId()+1);
+			qwerty.insertFileData(file);
 	}
 
 	@Override
 	public List<Board> selectArticleListByCategory(int categoryId, int page) {
-		// TODO Auto-generated method stub
-		return null;
+		int start = (page-1) *10;
+		return qwerty.selectArticleListByCategory(categoryId, start, start+10);
 	}
 
 	@Override
 	public List<Board> selectArticleListByCategory(int categoryId) {
-		// TODO Auto-generated method stub
-		return null;
+		return qwerty.selectArticleListByCategory(categoryId, 0, 100);
 	}
 
-	@Override
+	@Transactional
 	public Board selectArticle(int boardId) {
-		// TODO Auto-generated method stub
-		return null;
+		qwerty.updateReadCount(boardId);
+		return qwerty.selectArticle(boardId);
 	}
 
 	@Override
 	public BoardUploadFile getFile(int fileId) {
-		// TODO Auto-generated method stub
-		return null;
+		return qwerty.getFile(fileId);
 	}
 
-	@Override
+	@Transactional
 	public void replyArticle(Board board) {
-		// TODO Auto-generated method stub
-		
+		qwerty.updateReplyNumber(board.getMasterId(), board.getReplyNumber());
+		board.setBoardId(qwerty.selectMaxArticleNo()+1);
+		board.setReplyNumber(board.getReplyNumber()+1);
+		board.setReplyStep(board.getReplyStep()+1);
+		qwerty.replyArticle(board);
 	}
 
-	@Override
+	@Transactional
 	public void replyArticle(Board board, BoardUploadFile file) {
-		// TODO Auto-generated method stub
-		
+		qwerty.updateReplyNumber(board.getMasterId(), board.getReplyNumber());
+		board.setBoardId(qwerty.selectMaxArticleNo()+1);
+		board.setReplyNumber(board.getReplyNumber()+1);
+		board.setReplyStep(board.getReplyStep()+1);
+		qwerty.replyArticle(board);
+		if(file != null && file.getFileName() != null &&
+	!file.getFileName().equals("")) {
+			file.setBoardId(board.getBoardId());
+			qwerty.insertFileData(file);
+		}
 	}
 
 	@Override
 	public String getPassword(int boardId) {
-		// TODO Auto-generated method stub
-		return null;
+		return qwerty.getPassword(boardId);
 	}
 
 	@Override
 	public void updateArticle(Board board) {
-		// TODO Auto-generated method stub
-		
+		qwerty.updateArticle(board);
 	}
 
-	@Override
+	@Transactional
 	public void updateArticle(Board board, BoardUploadFile file) {
-		// TODO Auto-generated method stub
-		
+		qwerty.updateArticle(board);
+		if(file != null && file.getFileName() != null &&
+	!file.getFileName().equals("")) {
+			file.setBoardId(board.getBoardId());
+			
+			if(file.getFileId()>0) {
+				qwerty.updateFileData(file);
+			} else {
+				qwerty.insertFileData(file);
+			}
+		}
 	}
 
 	@Override
 	public Board selectDeleteArticle(int boardId) {
-		// TODO Auto-generated method stub
-		return null;
+		return qwerty.selectDeleteArticle(boardId);
 	}
 
-	@Override
+	@Transactional
 	public void deleteArticle(int boardId, int replyNumber) {
-		// TODO Auto-generated method stub
-		
+		if(replyNumber>0) {
+			qwerty.deleteReplyFileData(boardId);
+			qwerty.deleteArticleByBoardId(boardId);
+		} else if(replyNumber==0) {
+			qwerty.deleteFileData(boardId);
+			qwerty.deleteArticleByBoardId(boardId);
+		} else {
+			throw new RuntimeErrorException("WRONG_REPLYNUMBER");
+		}
 	}
 
 	@Override
 	public int selectTotalArticleCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return qwerty.selectTotalArticleCount();
 	}
 
 	@Override
 	public int selectTotalArticleCountByCategoryId(int categoryId) {
 		// TODO Auto-generated method stub
-		return 0;
+		return qwerty.selectTotalArticleCountByCategoryId(categoryId);
 	}
 
 	@Override
 	public List<Board> searchListByContentKeyword(String keyword, int page) {
-		// TODO Auto-generated method stub
-		return null;
+		int start = (page-1) * 10;
+		return qwerty.searchListByContentKeyword("%"+keyword+"%", start, start+10);
 	}
 
 	@Override
 	public int selectTotalArticleCountByKeyword(String keyword) {
-		// TODO Auto-generated method stub
-		return 0;
+		return qwerty.searchListByContentKeyword("%"+keyword+"%");
 	}
 
 }
